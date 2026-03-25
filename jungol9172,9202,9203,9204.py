c8f6aa5a9221bf56034636ce6ab5d34b55eac09a
byungkwon <ansqud14@gmail.com>
2026-03-25 17:54:02 +0900

    정올 연습문제

---
+++ jungol9172,9202,9203,9204.py
@@ -0,0 +1,26 @@
+# a=3
+# b=7
+
+# print(b, a)
+
+# a,b=map(int,input().split())
+
+# int1=a//b
+# int2=a%b
+
+# print(str(a),'/',str(b),'=',int1,'...',int2)
+
+# a,b,c=map(int,input().split())
+
+# print('sum =',a+b+c)
+# print('avg =',(a+b+c)//3)
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+f=int(input())
+g=int(input())
+
+print(a+2,b-2,c*2,d/2,e//2,f%2,g**2)
--- practice4.py
+++ practice4.py
@@ -1,5 +1,35 @@
-snack = '꿀꽈배기'
-print(len(snack))
+#snack = '꿀꽈배기'
+#print(len(snack))
 
+#print('꿀꽈배기는\n너무\n맛있어요')
 
-print('꿀꽈배기는\n너무\n맛있어요')
+# set1={5,7,6,2,9,0}
+# set2={3,4,4,5,6,7}
+
+# print(set1)
+# print(set2)
+# print(set1.intersection(set2))
+
+# list : 중복 o,순서 o, 수정 o 
+# tuple : 중복 o, 순서 o, 수정 X
+# set : 중복 x, 순서 x
+
+# my_list=['오예스','몽쉘','초코파이']
+# print(my_list[0])
+# my_list[0]='빅파이'
+# print(my_list)
+
+# my_tuple=('오예스','몽쉘','초코파이')
+# print(my_tuple[1])
+
+my_set={'돈가스','보쌈','제육덮밥'}
+# print(my_set[1])
+# my_set[1]='빅파이'
+my_set.add('닭갈비')
+print(my_set)
+my_set.remove('제육덮밥')
+print(my_set)
+my_set.clear()
+print(my_set)
+del my_set
+print(my_set)
