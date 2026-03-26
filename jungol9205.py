819ddcb086f6424b3fd5e4105742f14582c59bf7
byungkwon <ansqud14@gmail.com>
2026-03-26 11:16:51 +0900

    정올 언습문제제

---
+++ jungol9205.py
@@ -0,0 +1,10 @@
+num=int(input())
+
+# if num<=100:
+#     print(num)
+#     print(num+10)
+# else:
+#     print('100 이하의 숫자를 입력하세요')
+
+print(num)
+print(num+10)
---
+++ practice5.py
@@ -0,0 +1,21 @@
+# python='파이썬'
+# java='자바'
+
+# print('개발 언어에는 {},{} 등이 있어요'.format(python, java))
+
+# print('하지만 \'나만 당할 순 없지\'라는 생각에\"엄청 쉽지\"라고 했다.')
+
+# person={'이름':'나귀욤','나이':7,'키':120,'몸무게':23}
+# # print(person['이름'])
+# # print(person['나이'])
+# # #print(person['별명'])
+# # print(person.get('별명')) # 없는 key에 접근하면 None 출력
+# # print(person.get('이름')) 
+# person['최종학력']='유치원'
+# # person['키']=130
+# person.update({'키':140,'몸무게':26})
+# # person.pop('몸무게')
+# # person.clear()
+# print(person.keys())
+# print(person.values())
+# print(person.items())
