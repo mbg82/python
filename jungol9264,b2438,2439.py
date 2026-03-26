32e1cd56ab3a89e8cc71b7045e43b7d3ba545275
byungkwon <ansqud14@gmail.com>
2026-03-26 18:59:15 +0900

    b2439 추가가

---
+++ jungol9264,b2438,2439.py
@@ -0,0 +1,20 @@
+# n=int(input())
+
+# for i in range(2,n+1,2):
+#     print(i,end=' ')
+
+
+#b2438
+# m=int(input())
+
+# for i in range(m):
+    
+#     print('*'*(i+1))
+
+
+#b2439
+m=int(input())
+
+for i in range(1,m+1):
+    
+    print(' '*(m-1)+'*'*i)
