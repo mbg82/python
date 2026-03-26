62aefac6fd073dd5bb9d6c870d045512def37644
byungkwon <ansqud14@gmail.com>
2026-03-26 17:56:20 +0900

    b2739,2741,2742 및 jungol9256,9259

---
+++ b2739,2741,2742.py
@@ -0,0 +1,17 @@
+#b2739
+# n=int(input())
+
+# for m in range(1,10):
+#     print(n,'*',m,'=',n*m)
+
+#b2741
+n=int(input())
+
+for i in range(n):
+    print(i+1)
+
+#b2742
+m=int(input())
+
+for i in range(m,0,-1):
+    print(i)
---
+++ jungol9256,9259.py
@@ -0,0 +1,6 @@
+# for x in range(1,101):
+#     print(x,end=' ')
+
+
+for x in range(5,2001,5):
+    print(x,end=' ')
