6a7bb262bfe6464650ece105eaed9a8bdcd9ee20
byungkwon <ansqud14@gmail.com>
2026-03-27 18:56:40 +0900

    정올 및 백준 연습문제제

---
+++ jungol/jungol9196.py
@@ -0,0 +1,17 @@
+#jungol9196
+# school=input()
+# diff=input()
+# name=input()
+
+# print(f'{name} graduated {school} {diff} school.')
+
+#jungol9197
+# a,b,c=map(int,input().split())
+
+# print(f'{a} + {b} - {c} = {a+b-c}')
+
+#jungol9198
+a=float(input())
+b=float(input())
+
+print(f'total = {a+b}')
