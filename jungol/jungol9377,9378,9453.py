a6231ec9c400f0ed2991a4f6c2190c49e355cf59
byungkwon <ansqud14@gmail.com>
2026-03-31 18:13:50 +0900

    정올 문제풀이이

---
+++ jungol/jungol9377,9378,9453.py
@@ -0,0 +1,22 @@
+#9377
+# str=input()
+
+# print(*str)
+
+#9453
+# num=int(input())
+
+# def high():
+#     sum=num+10
+#     print(f'{num} + 10 = {sum}')
+# high()
+
+# def low():
+#     minus=num-10
+#     print(f'{num} - 10 = {minus}')
+# low()
+
+#9378
+str=input()
+
+print(*str[::2])
