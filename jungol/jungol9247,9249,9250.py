ac5692563c30499c6850566a01139c43f0026cbd
byungkwon <ansqud14@gmail.com>
2026-04-02 18:55:19 +0900

    정올 연습문제제

---
+++ jungol/jungol9247,9249,9250.py
@@ -0,0 +1,29 @@
+# 9247
+# 이름과 나이를 입력 받아 나이에 10을 더하여 출력하는 소스코드를 작성
+
+# a,b=input().split()
+
+# sum=int(b)+10
+
+# print(f'''name: {a} , age: {sum}''')
+
+
+#9249
+# 키와 몸무게를 입력 받아 출력하는 소스코드를 작성
+# h,w = map(int,input().split())
+
+# print(f'''{h} cm, {w} kg''')
+
+
+# """...."""
+
+#9250
+name1, age1 = input().split()
+name2, age2 = input().split()
+
+
+if age1 > age2:
+    print(name1)
+else:
+    print(name2)
+
