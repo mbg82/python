9067d259f8072a581ac5045fd5695577be32a3b0
byungkwon <ansqud14@gmail.com>
2026-04-07 10:19:47 +0900

    jungol 9290

---
+++ jungol/jungol9290.py
@@ -0,0 +1,35 @@
+#9290
+# n=int(input())
+
+# for i in range(0,n):
+#     print(f'Python {i}')
+
+# i=0
+
+# while i<n:
+#     print(f'Python {i}')
+#     i +=1
+
+
+# def inp():
+#     for i in range(0,n):
+#         print(f'Python {i}')
+
+# inp()
+
+#9291
+# n=input()
+
+# for i in range(20):
+#     print(n,end='')
+
+def inp():
+    n=input()
+
+    for i in range(20):
+        print(n,end='')
+
+inp()
+
+
+
