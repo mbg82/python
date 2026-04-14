5aff7adb963c28f64f8dc716b6a7ba3524558c04
byungkwon <ansqud14@gmail.com>
2026-04-14 14:31:10 +0900

    정올 연습문제

--- jungol/jungol9225,9228,9229,9230,9231,9232.py
+++ jungol/jungol9225,9228,9229,9230,9231,9232.py
@@ -24,32 +24,61 @@
 #     print("Middle School")
 
 #9230
-time=int(input())
+# time=int(input())
 
-if time <12:
-    print('AM')
-else:
-    print('PM')
+# if time <12:
+#     print('AM')
+# else:
+#     print('PM')
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-hour = int(input())
-# print(hour)
-is_pm = bool(hour // 12)
-print("PM" if is_pm else "AM")
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# hour = int(input())
+# # print(hour)
+# is_pm = bool(hour // 12)
+# print("PM" if is_pm else "AM")
 
 #9231
-# inp=int(input())
+inp=int(input())
 
-# if 90<=inp<=100:
-#     print('A')
-# elif 80<=inp<=89:
-#     print('B')
-# elif 70<=inp<=79:
-#     print('C')
-# elif 60<=inp<=69:
-#     print('D')
-# else:
-#     print('F')
+if 90<=inp<=100:
+    print('A')
+elif 80<=inp<=89:
+    print('B')
+elif 70<=inp<=79:
+    print('C')
+elif 60<=inp<=69:
+    print('D')
+else:
+    print('F')
+
+
+score=int(input())
+
+match score // 10:
+    case 10 | 9:
+        print('A')
+    case 8:
+        print("B")
+    case 7:
+        print("C")
+    case 6:
+        print("D")
+    case _:   # default와 같은 
+        print('F')
+
+
+score = int(input())
+
+criteria = {90: "A", 80: "B", 70: "C", 60: "D"}
+
+result = "F"
+for point, grade in criteria.items():
+    if score >= point:
+        result = grade
+        break
+
+print(result)
+
 
 #9232
 # weight=float(input())
