9f71d1ac3df2d4917ef181acad747bca093ae5cd
byungkwon <ansqud14@gmail.com>
2026-04-16 11:55:55 +0900

    정올

--- jungol/jungol9212,9213,9214.py
+++ jungol/jungol9212,9213,9214.py
@@ -54,40 +54,49 @@
 #     print(False)
 
 #9214
-a=0
-b=1
-c=2
+# a=0
+# b=1
+# c=2
 
-# 1. a and b
-if a and b:
-    print(False, end=' ')
-else:
-    print(True, end=' ')
+# # 1. a and b
+# if a and b:
+#     print(False, end=' ')
+# else:
+#     print(True, end=' ')
 
-# 2. b and c
-if b and c:
-    print(True, end=' ')
-else:
-    print(False, end=' ')
+# # 2. b and c
+# if b and c:
+#     print(True, end=' ')
+# else:
+#     print(False, end=' ')
 
-# 3. a or b
-if a or b:
-    print(False, end=' ')
-else:
-    print(True, end=' ')
+# # 3. a or b
+# if a or b:
+#     print(False, end=' ')
+# else:
+#     print(True, end=' ')
 
-# 4. b or c
-if b or c:
-    print(False, end=' ')
-else:
-    print(True, end=' ')
+# # 4. b or c
+# if b or c:
+#     print(False, end=' ')
+# else:
+#     print(True, end=' ')
 
-# 5. not a
-if not a:
-    print(True, end=' ')
-else:
-    print(False, end=' ')
+# # 5. not a
+# if not a:
+#     print(True, end=' ')
+# else:
+#     print(False, end=' ')
 
 #92146
 a,b,c = map(int,input().split())
 
+if a>b and a>c:
+    print(True)
+else:
+    print(False)
+
+if a == b == c:
+    print(True)
+else:
+    print(False)
