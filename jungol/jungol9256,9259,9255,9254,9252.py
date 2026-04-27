6dd06314f94078da114009ef60242a1d1daa20f4
byungkwon <ansqud14@gmail.com>
2026-04-27 19:40:32 +0900

    정올

--- jungol/jungol9256,9259,9255,9254,9252.py
+++ jungol/jungol9256,9259,9255,9254,9252.py
@@ -31,7 +31,29 @@
 # print(c)
 
 #9252
-a=5 
-a+=10
-a=a-1
-print(15)
+# a=5 
+# a+=10
+# a=a-1
+# print(15)
+
+#9251
+# score=int(input())
+
+# if score >=90:
+#     print('Perfect')
+# elif 80 <= score < 90:
+#     print('Great')
+# else:
+#     print("Effort")
+
+#9250
+name1, age1=input().split()
+name2, age2=input().split()
+
+age1=int(age1)
+age2=int(age2)
+
+if age1 > age2:
+    print(name1)
+else:
+    print(name2)
--- jungol/jungol9260,9261,9262,9263,9265,9266,9267,9268,9269,9270,9271,9272,9273,9274.py
+++ jungol/jungol9260,9261,9262,9263,9265,9266,9267,9268,9269,9270,9271,9272,9273,9274.py
@@ -114,23 +114,33 @@
 
 #9274
 
-n=int(input())
+# n=int(input())
 
-i=1
-sum=0
+# i=1
+# sum=0
 
-while i<=n:
-    sum +=i
-    i+=1
+# while i<=n:
+#     sum +=i
+#     i+=1
 
-print(sum)
+# print(sum)
 
 
-n=int(input())
+# n=int(input())
 
-sum=0 
+# sum=0 
 
-for i in range(1,n+1):
-    sum += i
+# for i in range(1,n+1):
+#     sum += i
     
-print(sum)
+# print(sum)
+
+
+#9276
+while True:
+    score = int(input())
+
+    if 0 <= score <= 100:
+        print(f"Score: {score}")
+    else:
+        break
