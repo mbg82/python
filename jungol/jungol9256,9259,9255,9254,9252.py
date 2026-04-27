c5e2bec30366d2570ebfa7a103b478ec16bf81dd
byungkwon <ansqud14@gmail.com>
2026-04-27 15:58:36 +0900

    정올 몇개

---
+++ jungol/jungol9256,9259,9255,9254,9252.py
@@ -0,0 +1,37 @@
+# for x in range(1,101):
+#     print(x,end=' ')
+
+
+# for x in range(5,2001,5):
+#     print(x,end=' ')
+
+
+#9255
+# import time 
+
+# now =time.localtime()
+# a=0
+# a=now.tm_year-1900
+# a+= now.tm_mon-1
+# a+=now.tm_mday
+# print(0,126,156)
+
+
+#9254
+# a,b=map(int,input().split())
+
+# print(a+b)
+# print(a*b)
+
+#9253
+# a,b=map(int,input().split())
+
+# c=max(a,b)-min(a,b)
+
+# print(c)
+
+#9252
+a=5 
+a+=10
+a=a-1
+print(15)
--- jungol/jungol9256,9259.py
+++
@@ -1,6 +0,0 @@
-# for x in range(1,101):
-#     print(x,end=' ')
-
-
-for x in range(5,2001,5):
-    print(x,end=' ')
