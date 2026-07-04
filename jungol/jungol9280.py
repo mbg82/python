8acf8ebc27bb0cdea4b50a2871177c543a04c381
byungkwon <ansqud14@gmail.com>
2026-07-04 13:50:00 +0900

    1111

---
+++ jungol/jungol9280.py
@@ -0,0 +1,9 @@
+N = int(input())
+
+for i in range(1, 5):
+    if i == N:
+        continue
+    print(i)
+
+
+    
---
+++ jungol/jungol9281.py
@@ -0,0 +1,11 @@
+total =0
+
+while True:
+    n=int(input())
+    if n == 0:
+        break
+    if n<=0:
+        continue
+    total +=n
+
+print(total)
